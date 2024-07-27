6f073eacd978f74aaa63ca5f7b8d6c0d0a271925
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2024-07-26 18:28:30 -0700

    v1.1.20

--- Assets/Forge/Scripts/Constants.cs
+++ Assets/Forge/Scripts/Constants.cs
@@ -307,7 +307,7 @@ public enum GameRegion
 
 public static class Constants
 {
-    public static readonly string ForgeVersion = "v1.1.19";
+    public static readonly string ForgeVersion = "v1.1.20";
 
     public static readonly int GameVersion = 4;
 

44ae05ae708699fdd2070a844e188eecc8aa36c7
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2024-08-28 07:45:14 -0700

    v2.0.0

--- Assets/Forge/Scripts/Constants.cs
+++ Assets/Forge/Scripts/Constants.cs
@@ -307,7 +307,7 @@ public enum GameRegion
 
 public static class Constants
 {
-    public static readonly string ForgeVersion = "v1.1.27";
+    public static readonly string ForgeVersion = "v2.0.0";
 
     public static readonly string RepoUrl = "https://github.com/Horizon-Private-Server/horizon-forge";
     public static readonly string WikiUrl = $"{RepoUrl}/wiki";
